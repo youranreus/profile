249563d96615c952b201eb8eaadcb6f822750b07
youranreus <youranreus@qq.com>
2021-12-29 13:53:34 +0800

    refactor: 将App.js重构为class组件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
+import React from "react";
 import './App.css';
 import {Layout} from "@douyinfe/semi-ui";
+import {getProfile} from "./api";
 
-function App() {
-    const {Header, Footer, Content, Sider} = Layout;
-    return (
-        <Layout>
-            <Sider className={"sider blocks"}>
-                Sidebar
-            </Sider>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profileData: {}
+        }
+    }
+
+    componentDidMount() {
+        getProfile().then(res => {
+            this.setState({
+                getProfile: res.data
+            })
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    render() {
+        const {Header, Footer, Content, Sider} = Layout;
+        return (
             <Layout>
-                <Header className={"blocks"}>
-                    Header
-                </Header>
-                <Content className={"blocks"}>
-                    Content
-                </Content>
-                <Footer className={"blocks"}>
-                    Footer
-                </Footer>
+                <Sider className={"sider blocks"}>
+                    Sidebar
+                </Sider>
+                <Layout>
+                    <Header className={"blocks"}>
+                        Header
+                    </Header>
+                    <Content className={"blocks"}>
+                        Content
+                    </Content>
+                    <Footer className={"blocks"}>
+                        Footer
+                    </Footer>
+                </Layout>
             </Layout>
-        </Layout>
-    );
+        );
+    }
 }
 
 export default App;
