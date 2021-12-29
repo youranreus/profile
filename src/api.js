0e555db6a1cfe1fd1bd5d02c9f34777d300d158b
youranreus <youranreus@qq.com>
2021-12-29 11:27:04 +0800

    update: 封装api

---
+++ src/api.js
@@ -0,0 +1,12 @@
+/**
+ * @author 季悠然
+ * @date 2021-12-29
+ */
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://i.exia.xyz/',
+    timeout: '2000'
+});
+
+export default api;
