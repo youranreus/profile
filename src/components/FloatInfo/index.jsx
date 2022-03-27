925e28c400c45c417ec7b04bb1c754d3306ac72c
youranreus <youranreus@qq.com>
2022-03-27 19:00:29 +0800

    feat: 侧边栏

--- src/components/FloatInfo/index.jsx
+++ src/components/FloatInfo/index.jsx
@@ -1,5 +1,6 @@
 import style from './style.module.scss'
-import {Avatar, Space, Tag} from '@douyinfe/semi-ui';
+import { Avatar, Space, Tag } from '@douyinfe/semi-ui';
+import { IconGithubLogo, IconMale, IconBookmark, IconMapPin } from '@douyinfe/semi-icons';
 
 function FloatInfo(props) {
 
@@ -43,8 +44,13 @@ function FloatInfo(props) {
                     </Space>
                 </div>
             </div>
-            <div className={style.block}>
-
+            <div className={[style.block, style.other].join(' ')}>
+                <ul>
+                    <li><IconMale size={"extra-large"} className={style.icon} style={{color: 'rgba(var(--semi-light-blue-2), 1)'}}/> <span className={style.text}>蓝孩纸</span></li>
+                    <li><IconBookmark size={"extra-large"} className={style.icon} style={{color: 'rgba(var(--semi-blue-5), 1)'}}/> <span className={style.text}>SZTU</span></li>
+                    <li><IconMapPin size={"extra-large"} className={style.icon} style={{color: 'rgba(var(--semi-cyan-6), 1)'}}/> <span className={style.text}>Shenzhen, China</span></li>
+                    <li><IconGithubLogo size={"extra-large"} className={style.icon}/> <span className={style.text}>@youranreus</span></li>
+                </ul>
             </div>
         </div>
     )
