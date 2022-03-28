4084dd2a91a82a143963317a8194a85b2ff733dc
youranreus <youranreus@qq.com>
2022-03-28 22:11:32 +0800

    feat: ProjectItem样式调整

--- src/components/ProjectItem/index.jsx
+++ src/components/ProjectItem/index.jsx
@@ -3,13 +3,14 @@
  * @date 2022-03-28
  */
 import style from './style.module.scss'
-import {Button, Col, Row} from '@douyinfe/semi-ui';
+import {Typography, Col, Row} from '@douyinfe/semi-ui';
 import {useEffect, useState} from "react";
 import axios from "axios";
 
 export default function ProjectItem(props) {
     const {title, desc, url, github} = props
     const [ghData, setGhData] = useState(null)
+    const {Text} = Typography
 
     useEffect(() => {
         axios.get('https://api.github.com/repos/' + github).then(res => {
@@ -32,20 +33,20 @@ export default function ProjectItem(props) {
                     <Col span={12}>
                         <div className={style.meta}>
                             <span>{ghData ? ghData.language : 'loading'}</span>
-                            { ghData ? <span> · {ghData.stargazers_count} stars</span> : '' }
+                            {ghData ? <span> · {ghData.stargazers_count} stars</span> : ''}
                         </div>
 
                     </Col>
-                    <Col span={12} style={{textAlign: 'right'}}>
+                    <Col span={12} style={{textAlign: 'right', position: "relative", zIndex: "3"}}>
                         {
-                            github ? (<Button onClick={() => {
-                                window.open('https://github.com/' + github, '_blank')
-                            }} theme='borderless'>Github</Button>) : ""
+                            github ? (<Text onClick={(e) => {
+                                e.stopPropagation()
+                            }} link={{href: 'https://github.com/' + github, target: "_blank"}}>Github</Text>) : ""
                         }
                         {
-                            github ? (<Button onClick={() => {
-                                window.open(url, '_blank')
-                            }} theme='borderless' style={{marginLeft: 5}}>访问</Button>) : ""
+                            url ? (<Text onClick={(e) => {
+                                e.stopPropagation()
+                            }} link={{href: url, target: "_blank"}} style={{marginLeft: 10}}>访问</Text>) : ""
                         }
                     </Col>
                 </Row>
